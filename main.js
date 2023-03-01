42454f6fc4d1d1c1bc3671b584e0bc02ac782168
Whoknow77 <gnsdh86166@gmail.com>
2023-03-01 13:25:38 +0900

    refactor: 주석 / 클린코드

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
 const todoInput = document.querySelector(".todo__input"); // todo 입력
 const todoList = document.querySelector(".todolist"); // todolist ul태그
 const addBtn = document.querySelector(".btn__add"); // 추가 버튼
-const allSelectBtn = document.querySelector(".btn__all__select");
-const allDeleteBtn = document.querySelector(".btn__all__delete");
+const allSelectBtn = document.querySelector(".btn__all__select"); // 전체 선택
+const allDeleteBtn = document.querySelector(".btn__all__delete"); // 전체 삭제
 
 let todos = [];
 let id = 0;
 
 const init = () => {
   paintTodos();
+
+  // 엔터로 할 일 추가
   todoInput.addEventListener("keypress", (e) => {
-    if (!e.target.value) return;
+    if (!e.target.value) return; // 공백일때는 추가 x
+
     if (e.key === "Enter") {
       appendTodos(e.target.value);
       todoInput.value = "";
     }
   });
 
+  // 버튼으로 할 일 추가
   addBtn.addEventListener("click", () => {
-    if (!todoInput.value) return;
+    if (!todoInput.value) return; // 공백일때는 추가 x
+
     appendTodos(todoInput.value);
     todoInput.value = "";
   });
 
+  // 전체삭제
   allDeleteBtn.addEventListener("click", () => {
     localStorage.clear();
     paintTodos();
   });
 
+  // 전체버튼
   allSelectBtn.addEventListener("click", () => {
     getAllTodos().forEach((todo) => {
-      completeTodo(todo.id);
+      selectTodo(todo.id);
     });
   });
 };
@@ -47,24 +54,27 @@ const getAllTodos = () => {
     : [];
 };
 
+// 할 일 추가
 const appendTodos = (text) => {
-  const oldtodos = getAllTodos();
+  const currenttodos = getAllTodos();
   const newId = id++;
-  const newTodos = { id: newId, isCompleted: false, content: text };
-  oldtodos.push(newTodos);
-  setTodos(oldtodos); // todo 업데이트
-  paintTodos(); // HTML에 그려주기
+  const newTodos = { id: newId, isSelected: false, content: text };
+  currenttodos.push(newTodos);
+  setTodos(currenttodos); // todo 업데이트
+  paintTodos();
 };
 
+// 할 일 삭제
 const deleteTodo = (todoId) => {
   const newTodos = getAllTodos().filter((todo) => todo.id !== todoId);
   setTodos(newTodos);
   paintTodos();
 };
 
-const completeTodo = (todoId) => {
+// 할 일 선택
+const selectTodo = (todoId) => {
   const newTodos = getAllTodos().map((todo) =>
-    todo.id === todoId ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    todo.id === todoId ? { ...todo, isSelected: !todo.isSelected } : todo
   ); // 같으면 toggle해서, 다르면 그대로 반환
   // spread 를 통해 객체의 데이터를 가져옴
 
@@ -73,12 +83,13 @@ const completeTodo = (todoId) => {
 };
 
 const editTodo = (e, todoId) => {
-  const oldtodoItem = getAllTodos().filter((todo) => todo.id === todoId);
+  const currentTodoItem = getAllTodos().filter((todo) => todo.id === todoId);
   const todoItem = e.target.parentNode; // li 태그
   const todotext = document.createElement("input");
-  todotext.value = oldtodoItem[0].content;
+  todotext.value = currentTodoItem[0].content; // 기존 input에 담겨있던 텍스트
   todotext.classList.add("edit__input");
 
+  // 엔터로 수정
   todotext.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
       updateTodo(e.target.value, todoId); // todo 수정
@@ -86,6 +97,7 @@ const editTodo = (e, todoId) => {
     }
   });
 
+  // todoItemElem 요소를 제외한 영역을 클릭 시, 수정모드 종료
   const onClickBody = (e) => {
     if (e.target.parentNode !== todoItem) {
       todoItem.removeChild(todotext);
@@ -93,7 +105,6 @@ const editTodo = (e, todoId) => {
     }
   };
 
-  // todoItemElem 요소를 제외한 영역을 클릭 시, 수정모드 종료
   document.body.addEventListener("click", onClickBody);
 
   todoItem.appendChild(todotext);
@@ -108,30 +119,32 @@ const updateTodo = (text, todoId) => {
   paintTodos();
 };
 
+// 화면에 그리기(render)
 const paintTodos = () => {
-  todoList.innerHTML = ""; // 초기화
-  const allTodos = getAllTodos(); // todo배열 가져오기
+  todoList.innerHTML = "";
+
+  const allTodos = getAllTodos();
 
   allTodos.forEach((todo) => {
-    // li
+    // <li>
+    //   <div(checkbox)> </div>
+    //    <div(text)> </div>
+    //    <button> </button>
+    //    <button> </button>
+    // </li>
+
     const todoItem = document.createElement("li");
     todoItem.classList.add("todo__item");
 
     // checkbox
     const checkbox = document.createElement("div");
     checkbox.classList.add("checkbox");
-    checkbox.addEventListener("click", () => completeTodo(todo.id));
+    checkbox.addEventListener("click", () => selectTodo(todo.id));
 
     // text
     const todotext = document.createElement("div");
     todotext.classList.add("todotext");
     todotext.innerText = todo.content;
-
-    // delete
-    const delBtn = document.createElement("button");
-    delBtn.classList.add("delBtn");
-    delBtn.addEventListener("click", () => deleteTodo(todo.id));
-    delBtn.innerText = "X";
 
     // edit
     const editBtn = document.createElement("button");
@@ -139,7 +152,13 @@ const paintTodos = () => {
     editBtn.addEventListener("click", (e) => editTodo(e, todo.id));
     editBtn.innerText = "Edit";
 
-    if (todo.isCompleted) {
+    // delete
+    const delBtn = document.createElement("button");
+    delBtn.classList.add("delBtn");
+    delBtn.addEventListener("click", () => deleteTodo(todo.id));
+    delBtn.innerText = "X";
+
+    if (todo.isSelected) {
       todoItem.classList.add("checked");
       checkbox.innerText = "✔";
     }
